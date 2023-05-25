900b3dc19e5038d7a3486fde47d7dfaf80a4d1cc
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-05-25 03:16:18 +0300

    fix gap between portfolio cards

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,17 @@
-const refs = {
-  openMenuBtn: document.querySelector(".menu-open"),
-  closeMenuBtn: document.querySelector(".menu-close"),
-  menu: document.querySelector(".mob-menu"),
-  body: document.querySelector("body"),
-};
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".menu-open"),
+    closeMenuBtn: document.querySelector(".menu-close"),
+    menu: document.querySelector(".mob-menu"),
+    body: document.querySelector("body"),
+  };
 
-refs.openMenuBtn.addEventListener("click", toggleMenu);
-refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-  refs.menu.classList.toggle("is-hidden");
-  refs.body.classList.toggle("no-scroll");
-  refs.body.classList.toggle("no-scroll");
-}
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
